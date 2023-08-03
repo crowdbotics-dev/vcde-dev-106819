96c68da4c993914d9c87492749c5b92be067272e
Crowdbotics <engineering@crowdbotics.com>
2023-08-03 05:29:49 +0000

    Initial commit.

---
+++ backend/vcde_dev_106819/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for vcde_dev_106819 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vcde_dev_106819.settings')
+
+application = get_wsgi_application()
